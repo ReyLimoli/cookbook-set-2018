3024cae131bbaa8ed85894650fa92c838fb79677
Reynaldo e Antônio <reynaldo.limoli@gmail.com>
2018-09-19 19:40:35 -0300

    Usuário se autentica no site - login e logout

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -19,4 +19,25 @@ feature 'User login' do
     expect(page).to have_link('Sair')
     expect(page).to have_content('Olá reynaldo@gmail.com')
   end
+  
+  scenario 'User logout' do
+    User.create(email: 'reynaldo@gmail.com', password: '12345678')
+
+    visit root_path
+
+    click_on 'Entrar'
+
+    fill_in "Email", with: "reynaldo@gmail.com"
+    fill_in "Senha", with: "12345678"
+    within '.actions' do
+      click_on 'Entrar'
+    end
+    
+    click_on 'Sair'
+
+    expect(current_path).to eq root_path
+    expect(page).not_to have_link('Sair')
+    expect(page).to have_link('Entrar')
+    
+  end
 end
